53cdb7dc12f5bbd38c84e4c726c6f95f0dceb2b6
Hunter Jones <hdog0915@gmail.com>
2023-06-04 23:55:54 -0400

    Add routing
    Add routing page with at the moment just education/about me and work experience

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WorkExperienceComponent } from './tabs/work-experience/work-experience.component';
+import { EducationComponent } from './tabs/education/education.component';
+import { ContactMeComponent } from './tabs/contact-me/contact-me.component';
 
-const routes: Routes = [];
+const routes: Routes = 
+[
+  {path: 'work-experience', component: WorkExperienceComponent},
+  {path: 'education', component: EducationComponent},
+  // {path: 'about-me', component: ContactMeComponent},
+  { path: '**', redirectTo: '/work-experience'},
+  {path: '', redirectTo: 'work-experience', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
