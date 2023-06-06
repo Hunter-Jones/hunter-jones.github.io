c5a13632a1c6b4abad99fc69739d5b33cc4b446d
Hunter Jones <hdog0915@gmail.com>
2023-06-05 22:46:16 -0400

    Add start of projects section
    Create start of projects section featuring boxes for each project including a visualization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { WorkExperienceComponent } from './tabs/work-experience/work-experience.component';
 import { EducationComponent } from './tabs/education/education.component';
 import { ContactMeComponent } from './tabs/contact-me/contact-me.component';
+import { CsProjectsComponent } from './tabs/cs-projects/cs-projects.component';
+import { DataScienceProjectsComponent } from './tabs/data-science-projects/data-science-projects.component';
 
 const routes: Routes = 
 [
   {path: 'work-experience', component: WorkExperienceComponent},
   {path: 'education', component: EducationComponent},
+  {path: 'projects', component: CsProjectsComponent},
+  // {path: 'cs-projects', component: CsProjectsComponent},
+  // {path: 'data-projects', component: DataScienceProjectsComponent},
   // {path: 'about-me', component: ContactMeComponent},
   { path: '**', redirectTo: '/work-experience'},
   {path: '', redirectTo: 'work-experience', pathMatch: 'full'}
